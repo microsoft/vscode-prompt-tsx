7f459ad92be8b106d5b0edd716913ff1557660ac
Joyce Er <joyce.er@microsoft.com>
2024-05-24 14:49:03 -0700

    Simplify further

--- build/postcompile.ts
+++ build/postcompile.ts
@@ -2,16 +2,12 @@
  *  Copyright (c) Microsoft Corporation and GitHub. All rights reserved.
  *--------------------------------------------------------------------------------------------*/
 
-import * as fs from 'fs';
 import * as path from 'path';
 import { copyStaticAssets } from './postinstall';
 
-const REPO_ROOT = path.join(__dirname, '..');
-
 async function main() {
-	// Ship the tiktoken file in the dist bundle
+	// Ship the vscodeTypes.d.ts file in the dist bundle
 	await copyStaticAssets([
-		'src/base/index.d.ts',
 		'src/base/vscodeTypes.d.ts',
 	], 'dist/base/');
 }
